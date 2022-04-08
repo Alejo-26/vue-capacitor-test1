7f649580f38ec7593c7c7bace61d72a51ec9c014
Alejandro <luisalejo2699@gmail.com>
2022-04-08 21:25:48 +0200

    discover repository

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,10 @@
   <div>
     <ion-card>
       <ion-item>
-        <ion-label position="floating"> Hello </ion-label>
+        <ion-label position="floating"> Hello2 </ion-label>
         <ion-input></ion-input>
       </ion-item>
-      <ion-button color="tertiary"> Hello </ion-button>
+      <ion-button color="tertiary"> Hello2 </ion-button>
     </ion-card>
   </div>
 </template>
